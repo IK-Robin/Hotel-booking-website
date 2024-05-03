6d8a17cf9cd2d2f8d2251799ce80e6dd80568421
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-05-03 11:49:24 +0600

    update css

--- inc/links.php
+++ inc/links.php
@@ -1,5 +1,3 @@
-<!-- style  css  -->
-<link rel="stylesheet" href="./css/style.css">
 
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -13,6 +11,8 @@
 
 <!-- bootstrap icon  -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<!-- style  css  -->
+<link rel="stylesheet" href="./css/style.css">
 
 
 
