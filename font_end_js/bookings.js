4f9f554670f4a5a5f3e71ff30e930e2967d346d8
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-05-21 09:05:19 +0600

    "Added booking form to room details page, removed features and facilities sections, and updated layout and styling."

---
+++ font_end_js/bookings.js
@@ -0,0 +1,14 @@
+// add carosal js  
+var swiper = new Swiper(".rooms_slider", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
