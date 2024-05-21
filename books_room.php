0aff212c51e7b1e4b29c4603f91033b7f8ec83df
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-05-21 09:53:41 +0600

    "Updated books_room.php and bookings.js files: added onchange event to date inputs, removed room description and list of rooms, and added JavaScript function to check room availability using AJAX."

--- books_room.php
+++ books_room.php
@@ -134,14 +134,14 @@
                   <div class="mb-3">
                     <label f class="form-label">Chekcout date
                     </label>
-                    <input type="date" name="checkin" class="form-control shadow-none"  />
+                    <input type="date" onchange="check_rooms_avabilable()" name="checkin" class="form-control shadow-none"  />
                   </div>
                 </div>
                 <div class="col-lg-6">
                   <div class=" mb-3">
                     <label for="checkoutDate" class="form-label">Chekcout date
                     </label>
-                    <input type="date" name="checkout" class="form-control shadow-none"  />
+                    <input onchange="check_rooms_avabilable()" type="date" name="checkout" class="form-control shadow-none"  />
                   </div>
                 </div>
 
@@ -167,13 +167,7 @@
 
         </div>
       </div>
-      <div class="col-lg-12">
-        <!-- add discription  -->
-        <div class="discription mt-4">
-          <h6 class="mb-1">Description</h6>
-          <p><?php echo $room['rooms_desc']; ?></p>
-        </div>
-      </div>
+    
     </div>
   </div>
 
@@ -190,74 +184,6 @@
 
         <?php
 
-        // Fetch rooms that are active and not removed
-// $res = select("SELECT * FROM `rooms` WHERE `status`=? AND `remove`=?", [1, 0], 'ii');
-        
-        // $rooms_path = ROOM_IMG_PATH;
-// while ($row = mysqli_fetch_assoc($res)) {
-//     // Fetch the room ID from the current room row
-//     $room_id = $row['id']; // Assuming there's an 'id' column in your `rooms` table
-        
-        //     // Fetch facilities for the current room
-//     $rooms_facilities = mysqli_query($conn, "SELECT f.name FROM `facilities` f INNER JOIN `room_facility` rfec ON f.id = rfec.facility_id WHERE rfec.rooms_id = $room_id");
-        
-        //     // Loop through the facilities and collect them
-//     $facilities = "";
-//     while ($room_f = mysqli_fetch_assoc($rooms_facilities)) {
-//         $facilities .= "<span class='badge rounded-pill bg-light text-dark text-wrap'>{$room_f['name']}</span> ";
-//     }
-        
-        //     // Fetch features for the current room
-//     $room_featuress = mysqli_query($conn, "SELECT feature.name FROM `feature_facility` feature INNER JOIN `room_featurs` rFeaturs ON feature.id = rFeaturs.featurs_id WHERE rFeaturs.room_id = $room_id");
-        
-        //     // Loop through the features and collect them
-//     $featurss = "";
-//     while ($room_feat = mysqli_fetch_assoc($room_featuress)) {
-//         $featurss .= "<span class='badge rounded-pill bg-light text-dark text-wrap'>{$room_feat['name']}</span> ";
-//     }
-        
-        //     // Get room's image
-//     $room_image = mysqli_query($conn, "SELECT * FROM `rooms_images` WHERE `rooms_id` = $room_id AND `thumb` = 1");
-//     $room_img_path = ROOM_IMG_PATH . 'thumbnail.jpg';
-//     if (mysqli_num_rows($room_image) > 0) {
-//         $room_img_paths = mysqli_fetch_assoc($room_image);
-//         $room_img_path = ROOM_IMG_PATH . $room_img_paths['img_name'];
-//     }
-        
-        //     // Output the collected data
-        
-
-        //     // Output the room card
-//     echo <<<DATA
-//     <div class="card mb-3 border-0 shadow">
-//         <div class="row g-3 p-3 align-items-center">
-//             <div class="col-md-5">
-//                 <img src="$room_img_path" class="w-100 rounded" alt="Room Image">
-//             </div>
-//             <div class="col-md-5">
-//                 <div class="card-body p-0">
-//                     <h5 class="card-title">{$row['rooms_names']}</h5>
-//                     <!-- Features -->
-//                     <div class="features mb-4">
-//                         <h6 class="mb-1">Features</h6>
-//                         $featurss
-//                     </div>
-//                     <!-- Facilities -->
-//                     <div class="facilities mb-4">
-//                         <h6 class="mb-1">Facilities</h6>
-//                         $facilities
-//                     </div>
-//                 </div>
-//             </div>
-//             <div class="col-md-2 text-center">
-//                 <h6 class="mb-4">$$row[price] per night</h6>
-//                 <a href="#" class="btn btn-sm w-100 text-white custom_bg mb-2">Book Now</a>
-//                 <a href="rooms_detail.php" class="btn btn-sm w-100 btn-outline-dark shadow-none">More Details</a>
-//             </div>
-//         </div>
-//     </div>
-// DATA;
-// }
         
         ?>
 
@@ -286,4 +212,4 @@
   <script src="./font_end_js/bookings.js"></script>
 </body>
 
-</html>
+</html> 
